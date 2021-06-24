e3643f5ccc2be2beefc10c10af7b075982d89ab6
Shota Iwami <saxo-folatre1129@outlook.jp>
2021-06-25 03:49:26 +0900

    update

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,7 +7,10 @@ module.exports = {
       // url: ["http://localhost:1313/blog/"]
     },
     upload: {
-      target: "temporary-public-storage",
+      // target: "temporary-public-storage",
+      target: "lhci",
+      serverBaseUrl: "https://lhci-heroku-biwashi.herokuapp.com",
+      token: "16f83bd7-013b-4c47-a1cd-538f5764bfc4",
     },
   },
 };
