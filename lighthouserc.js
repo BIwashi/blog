1e3875b49768a03c56130154f7da6aeb89814530
Shota Iwami <saxo-folatre1129@outlook.jp>
2021-06-25 03:33:55 +0900

    update

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,9 +2,9 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 2,
-      // staticDistDir: "./docs",
-      startServerCommand: "hugo server",
-      url: ["http://localhost:1313/blog/"]
+      staticDistDir: "./docs",
+      // startServerCommand: "hugo server",
+      // url: ["http://localhost:1313/blog/"]
     },
     upload: {
       target: "temporary-public-storage",
