43ac9ced058294006b502e8156cd449a716e7e46
Shota Iwami <saxo-folatre1129@outlook.jp>
2021-06-25 12:36:46 +0900

    update

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,7 +5,7 @@ module.exports = {
       staticDistDir: "./docs",
     },
     upload: {
-      // target: "temporary-public-storage",
+      // target: "temporary-public-storage", 
       target: "lhci",
       serverBaseUrl: "https://lhci-heroku-biwashi.herokuapp.com",
       token: "16f83bd7-013b-4c47-a1cd-538f5764bfc4",
